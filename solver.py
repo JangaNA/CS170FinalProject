7a9ccf1218ae621a7cb092f23a12a73264b3db94
unknown <andy.zhang@berkeley.edu>
2017-11-19 01:57:09 -0800

    end of the knave

--- solver.py
+++ solver.py
@@ -1,62 +1,87 @@
 import argparse
 import random
+import threading
 from multiprocessing.dummy import Pool as ThreadPool 
 """
 ======================================================================
   Complete the following function.
 ======================================================================
 """
-
 def solve(num_wizards, num_constraints, wizards, constraints):
     """
     Write your algorithm here.
     Input:
-        num_wizards: Number of wizards
-        num_constraints: Number of constraints
-        wizards: An array of wizard names, in no particular order
-        constraints: A 2D-array of constraints, 
-                     where constraints[0] may take the form ['A', 'B', 'C']i
+     num_wizards: Number of wizards
+     num_constraints: Number of constraints
+     wizards: An array of wizard names, in no particular order
+     constraints: A 2D-array of constraints, 
+            where constraints[0] may take the form ['A', 'B', 'C']i
 
     Output:
-        An array of wizard names in the ordering your algorithm returns
+     An array of wizard names in the ordering your algorithm returns
     """
-    def solve_helper(n):
+    def all_perms(elements):
+        if len(elements) <=1:
+            yield elements
+        else:
+            for perm in all_perms(elements[1:]):
+                for i in range(len(elements)):
+                    yield perm[:i] + elements[0:1] + perm[i:]
+
+    perms=all_perms(wizards)
+    # finds best ordering in a random sample
+    final=[]
+    def solve_helper():
         outputs=[]
         sat=[]
         output=wizards
-        for i in range(1000000):
+        for i in range(1000):
             output=random.sample(output,len(wizards))
             if output not in outputs:
                 outputs.append(output)
                 sat.append(numSat(constraints,output)) 
-        return (max(sat),outputs[sat.index(max(sat))])
-    pool = ThreadPool(80)
-    results=solve_helper(1)
-    results = pool.map(solve_helper,[i for i in range(100)])
-    print(results)
-    maximum=0
+        final.append([max(sat),outputs[sat.index(max(sat))]])
+
+    #mulithreading
+    # numThreads = 10
+    # threadList=[]
+    # print("Starting...\n")
+    # for i in range(numThreads):
+    #     t=threading.Thread(target=solve_helper,args=(1,))
+    #     t.start()
+    #     threadList.append(t)
+    # print("\nThread Count: "+str(threading.activeCount()))
+    # print("Exiting...\n")
+
+    #knave
+    for i in range(100):
+        solve_helper()
+
+    # #finds best in final
+    maxsat=0
     output=[]
-    for elem in results:
-        if elem[0]>maximum:
-            maximum=elem[0]
-            output=elem[2]
+    for elem in final:
+        if elem[0]>maxsat:
+            maxsat=elem[0]
+            output=elem[1]
+    print(maxsat,output)
     return output
 def numSat(constraints,output):
     satisfied=0
     for cond in constraints:
-            if not inRange(cond,output):
-                satisfied+=1
+         if not inRange(cond,output):
+          satisfied+=1
     return satisfied
 def inRange(cond,output):
     first=output.index(cond[0])
     second=output.index(cond[1])
     subject=output.index(cond[2])
     if first<second:
-        if subject in range(first,second):
-            return True
+     if subject in range(first,second):
+         return True
     if second<first:
-        if subject in range(second,first):
-            return True
+     if subject in range(second,first):
+         return True
     return False
 """
 ======================================================================
@@ -66,23 +91,23 @@ def inRange(cond,output):
 
 def read_input(filename):
     with open(filename) as f:
-        num_wizards = int(f.readline())
-        num_constraints = int(f.readline())
-        constraints = []
-        wizards = set()
-        for _ in range(num_constraints):
-            c = f.readline().split()
-            constraints.append(c)
-            for w in c:
-                wizards.add(w)
-                
+     num_wizards = int(f.readline())
+     num_constraints = int(f.readline())
+     constraints = []
+     wizards = set()
+     for _ in range(num_constraints):
+         c = f.readline().split()
+         constraints.append(c)
+         for w in c:
+          wizards.add(w)
+          
     wizards = list(wizards)
     return num_wizards, num_constraints, wizards, constraints
 
 def write_output(filename, solution):
     with open(filename, "w") as f:
-        for wizard in solution:
-            f.write("{0} ".format(wizard))
+     for wizard in solution:
+         f.write("{0} ".format(wizard))
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description = "Constraint Solver.")
