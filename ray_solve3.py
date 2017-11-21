568945703bbcf35c2953bd7c30cc1fc344b04a7c
unknown <andy.zhang@berkeley.edu>
2017-11-20 19:58:43 -0800

    fixed numSat

--- ray_solve3.py
+++ ray_solve3.py
@@ -31,48 +31,45 @@ def solve(num_wizards, num_constraints, wizards, constraints):
     loop_counter = 0
     prev_max = 0
     while not numSat(constraints, assignment,0) == len(constraints):
-        print("numSat", numSat(constraints, assignment, 0))
+        print("numSat", numSat(constraints, assignment, num_constraints))
         loop_counter += 1
         if loop_counter % 10 == 0:
             print("loops",loop_counter)
             print("time taken", time.time()-start)
-	scores = {}
-	#unsatisfied = unsatisfiedConstraints(constraints, assignment)
-	#wizUnsat = set()
-	#for constraint in unsatisfied:
-	#    wizUnsat.add(constraint[0])
-	#print("num wixUnsat", len(wizUnsat))
-	max_sat = 0
+        scores = {}
+        max_sat = 0
         for i in range(len(wizards)):
             for j in range(len(wizards)):
-                if (i != j) and ((j,i) not in scores):
+                if (i != j) and (j,i) not in scores:
                     temp = assignment[i]
                     assignment[i] = assignment[j]
                     assignment[j] = temp
                     score = numSat(constraints,assignment,max_sat)
                     scores[(i,j)] = score
-		    temp = assignment[i]
+                    temp = assignment[i]
                     assignment[i] = assignment[j]
                     assignment[j] = temp
-		    if scores[(i,j)] > max_sat:
-			max_sat = score
+                    if scores[(i,j)] > max_sat:
+                        max_sat = score
+
         max_score_swap = max(scores, key=scores.get)
         if max_score_swap == prev_max:
             print("RESHUFFLING")
             random.shuffle(assignment)
         prev_max = max_score_swap
-	prev_score = scores[max_score_swap]
+        prev_score = scores[max_score_swap]
         temp = assignment[max_score_swap[0]]
         assignment[max_score_swap[0]] = assignment[max_score_swap[1]]
         assignment[max_score_swap[1]] = temp
-    print("Solved BITCH! numSat:", numSat(constraints, assignment))
+    print("Solved BITCH! numSat:", numSat(constraints, assignment,num_constraints))
+    print("Time taken:",time.time()-start)
     return assignment
 
 def unsatisfiedConstraints(constraints, output):
     unsatisfied = []
     for cond in constraints:
-	if inRange(cond,output):
-	    unsatisfied.append(cond)
+        if inRange(cond,output):
+            unsatisfied.append(cond)
     return unsatisfied
 
 def numSat(constraints,output,currMax):
@@ -80,10 +77,10 @@ def numSat(constraints,output,currMax):
     remaining=len(constraints)
     for cond in constraints:
         if satisfied + remaining < currMax:
-	    return 0
+            return 0
         if not inRange(cond,output):
             satisfied+=1
-	remaining += 1
+        remaining-=1
     return satisfied
 #returns true if 3rd element between first two
 def inRange(cond,output):
