de8c5ed29a8b3401d238cbdb430096f02405f72f
Raymond Feng <raymondhfeng@berkeley.edu>
2017-11-29 23:20:40 -0800

    140 solved

--- ray_solve2.py
+++ ray_solve2.py
@@ -33,7 +33,7 @@ def solve(num_wizards, num_constraints, wizards, constraints):
             return num_constraints-numSat(constraints,self.state)
     init_state=wizards#find_best_rand(10000)
     wp=WizardProblem(init_state)
-    auto_schedule=wp.auto(minutes=1,steps=100000)
+    auto_schedule=wp.auto(minutes=0.1,steps=20000)
     wp.set_schedule(auto_schedule)
     wp.copy_strategy="slice"
     state,e= wp.anneal()
